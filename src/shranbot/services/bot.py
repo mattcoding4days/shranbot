60be044b5f96183e46996bde27cc6285a0c1c984
Matt Williams <mattinzhuhai@outlook.com>
2021-11-15 22:01:06 -0800

    :octocat: Added rust like Result library, for more elegant error handling

--- src/shranbot/services/bot.py
+++ src/shranbot/services/bot.py
@@ -1,8 +1,9 @@
 """
-Arb doc
+Discord bot actor
 """
 
 import discord
+from result import Ok, Err, Result
 from shranbot import Config
 from shranbot.logging.pkg_logger import Logger
 
@@ -14,14 +15,24 @@ class HerbertWest(discord.Client):
     The actor
     """
 
+    async def guild_name(self) -> Result[str, str]:
+        """
+        @description: retrieve the guild name
+        """
+        active_guild = Err("No active server was found")
+        for guild in self.guilds:
+            if guild.name == Config.discord_guild():
+                active_guild = Ok(str(guild.name))
+                break
+
+        return active_guild
+
     async def on_ready(self):
         """
         @description: test method
         """
-        active_guild = None
-        for guild in self.guilds:
-            if guild.name == Config.discord_guild():
-                active_guild = guild.name
-                break
-
-        Log.info("Logged on as %s in server %s", self.user, active_guild)
+        gname = await self.guild_name()
+        if gname.is_ok():
+            Log.info("Logged on as %s in server %s", self.user, gname.value)
+        else:
+            Log.error("%s", gname.value)
