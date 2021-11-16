940cfc742c52db97ea5453ea334a7a22c7d8d285
01000100W <67023635+01000100W@users.noreply.github.com>
2021-11-16 00:13:20 -0800

    Volume
    Added in volume data, after spending way too long reading documentation while high to notice its the same call just change the object...

--- src/shranbot/services/tickers/dmgi.py
+++ src/shranbot/services/tickers/dmgi.py
@@ -17,3 +17,11 @@ class Dmgi:
         """
         todays_data = self.dmgi.history(period='1d')
         return "DMGI closed at {}{:0.2f} CAD".format(emoji.emojize(":heavy_dollar_sign:"), todays_data['Close'][0])
+    
+    def todays_trading_volume(self) -> str:
+        """
+        @description: get the trading volume of today/last trading day
+        """
+        todays_data = self.dmgi.history(period='1d')
+        return "DMGI's volume was {}{:0.2f} shares traded".format(emoji.emojize(":chart_with_upwards_trend:"), todays_data['Volume'][0])
+    
